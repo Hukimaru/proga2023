c466c0f1a243c4d5e09a7fed3a3c8744625eacf7
Учетка Величайшего <f.rofin2016@yandex.ru>
2022-10-28 11:35:32 +0300

    Бред какой-то

--- 1_draw.py
+++ 1_draw.py
@@ -7,6 +7,7 @@ FPS = 30
 screen = pygame.display.set_mode((400, 400))
 
 rect(screen, (255, 0, 255), (100, 100, 200, 200))
+print('rt')
 rect(screen, (0, 0, 255), (100, 100, 200, 200), 5)
 polygon(screen, (255, 255, 0), [(100,100), (200,50),
                                (300,100), (100,100)])
